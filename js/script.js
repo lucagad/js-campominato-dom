9855527587893d0b9ba3765bc3c0c34bf93da09c
lucagad <lucagaddini@icloud.com>
2022-04-22 16:33:08 +0200

    {JS} Modified: Square with external number and icon

--- js/script.js
+++ js/script.js
@@ -1,27 +1,6 @@
 // Esercizio di oggi: **Campo Minato**
 // nome repo: js-campominato-dom
-// **Consegna**
-// Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell’esercizio ma solo l’index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l’inizializzazione di git).
-// ****L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
-// con difficoltà 1 => tra 1 e 100
-// con difficoltà 2 => tra 1 e 81
-// con difficoltà 3 => tra 1 e 49
-// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
-// I numeri nella lista delle bombe non possono essere duplicati.
-// In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
-// La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
-// **BONUS:**
-// 1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
-// ****2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
-
-// **Consigli del giorno:**
-// ****Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-// Ad esempio:
-// Di cosa ho bisogno per generare i numeri?
-// Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
-// Le validazioni e i controlli possiamo farli anche in un secondo momento.
 
 const containerGame = document.querySelector('.container_game_grid');
 console.log(containerGame);
@@ -100,7 +79,7 @@ function handleClickCell(){
   if(bombs.includes(this.cellNumber)){
     let mySound = new Audio('audio/bomb.mp3');
     mySound.play();
-    this.innerHTML = `<img src="img/bomb.png" alt=""></img>`;
+    this.innerHTML = `<img class="bomb_icon" src="img/bomb.png" alt=""></img>`;
     this.classList.add('bomb');
     gameOver = true;
     endGame(gameOver);
@@ -108,12 +87,13 @@ function handleClickCell(){
   } else {
     let mySound = new Audio('audio/flower.mp3');
     mySound.play();
-    this.innerHTML = `<img src="img/flower.png" alt=""></img>`;
+    this.innerHTML = `<img class="flower_icon" src="img/flower.png" alt=""></img>`;
     this.classList.add('flower');
     clickNumber ++;
-    console.log('Numero Click',clickNumber);
 
+    console.log('Numero Click',clickNumber);
     console.log('Numero Massimo di Click',numberOfCells-BOMBS_NUMBER);
+
     if(clickNumber === (numberOfCells-BOMBS_NUMBER)){
     endGame(gameOver);  
     }
@@ -137,7 +117,6 @@ function endGame(gameOver){
   }
   
   
-
   // for (let i = 0; i < BOMBS_NUMBER; i++){
   //   const bombId = bombs[i];
   //   console.log(bombId);
@@ -161,10 +140,14 @@ function createSquare(target,dimension,cellId){
   const newSquare = document.createElement('div');
 
   newSquare.className = 'square'+dimension;
+
+  newSquare.classList.add('d-flex','flex-column', 'justify-content-center', 'align-content-center');
+
   const number = getUniqueRandomNumber(1,dimension);
 
-  newSquare.innerHTML = `<span class="square_number">${cellId+1}</span>`;
+  newSquare.innerHTML = `<img src="https://yt3.ggpht.com/ytc/AKedOLTO2XVhtyMr24Dnz6QJ-Lsj_05XHim-qMoF6PRc=s900-c-k-c0x00ffffff-no-rj" alt="" height="15px" class="logo_card">`;
 
+  newSquare.innerHTML += `<span class="square_number">${cellId+1}</span>`;
   // creo la proprietà cellNumber per andarla a leggere al click
   newSquare.cellNumber = cellId;
 
